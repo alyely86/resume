5cb11c568146f91174fdc3d4f740d7137eee9990
aly <alyasyly868@gmail.com>
2024-12-06 21:51:34 -0800

    add inheritance in template and add my langueges

--- blog_app/views.py
+++ blog_app/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def resume_view(request):
-    return render(request,'index.html')
+    phone_number = '09015487434'
+    email = 'alyasyly8668@gmail.com'
+    return render(request,'index.html',{'email':email,'phone_number':phone_number})
