07ba1b43a4229b9a3005e5df3238c6bd29961217
aly <alyasyly868@gmail.com>
2024-12-10 09:09:11 -0800

    add blog template

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('',resume_view,name='resume')
+    path('',post_blog,name='post_blog'),
+    path('resume/',resume_view,name='resume')
 ]
 
--- blog_app/views.py
+++ blog_app/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+def post_blog(request):
+    return render(request,'blog/index.html')
+
+# for resume
 def resume_view(request):
     phone_number = '09015487434'
     email = 'alyasyly8668@gmail.com'
-    return render(request,'index.html',{'email':email,'phone_number':phone_number})
+    return render(request,'resume/index.html',{'email':email,'phone_number':phone_number})
+
